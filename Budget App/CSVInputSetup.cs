2dd5799132b76da7a313d41daddc217ce8aa6129
Brad Evans <evansbradleym@gmail.com>
2019-02-12 07:17:37 -0600

    Updates to CSV input setup
    Making incremental progress on the csv setup

--- Budget App/CSVInputSetup.cs
+++ Budget App/CSVInputSetup.cs
@@ -12,6 +12,8 @@ namespace Budget_App
 {
     public partial class CSVInputSetup : Form
     {
+        private List<string> lines = new List<string>();
+
         public CSVInputSetup()
         {
             InitializeComponent();
@@ -23,18 +25,32 @@ namespace Budget_App
 
             using (StreamReader sr = new StreamReader(filename))
             {
-
+                string line = string.Empty;
+                while(!string.IsNullOrEmpty(line = sr.ReadLine()) && lines.Count < 6)
+                {
+                    lines.Add(line);
+                }
             }
+
+            UpdateGridView();
         }
 
         private void txtDelimiter_TextChanged(object sender, EventArgs e)
         {
+            UpdateGridView();
+        }
 
+        private void UpdateGridView()
+        {
+            dgvInputSetup.AutoGenerateColumns = true;
+            List<string[]> splitLines = lines.Select(l => l.Split(new string[] { txtDelimiter.Text }, StringSplitOptions.None)).ToList();
+            dgvInputSetup.DataSource = splitLines;
+            dgvInputSetup.Refresh();
         }
 
         private void cmdSave_Click(object sender, EventArgs e)
         {
-
+            // Save the ImportSpec
         }
     }
 }
